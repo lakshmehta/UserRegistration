bf14f7d84b66bae8b66c3666cc223a26aaba1dbf
unknown <lakshmehta06@gmail.com>
2021-04-12 12:01:12 +0530

    [Laksh added UC2]

--- UserRegisatration/UserRegisatration/Program.cs
+++ UserRegisatration/UserRegisatration/Program.cs
@@ -14,6 +14,9 @@ namespace UserRegisatration
             Console.WriteLine("Enter First_Name:");
             string firstName = Console.ReadLine();
             Console.WriteLine(patter.isValidFirstName(firstName));
+            Console.WriteLine("Enter Last_Name:");
+            string lastName = Console.ReadLine();
+            Console.WriteLine(patter.isValidFirstName(lastName));
 
         }
     }
